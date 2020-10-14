add8ee6133dd01bd2b56fda9ce5297411708ec27
unknown <geovannarenata17@gmail.com>
2020-10-14 17:16:42 -0300

    Implementações Front End

--- PlanteUmaArvore-Front-Pi/src/app/app-routing.module.ts
+++ PlanteUmaArvore-Front-Pi/src/app/app-routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { ContatoComponent } from './contato/contato.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
-  {
-    path: '', redirectTo: 'home', pathMatch: 'full'
-  },
-  {
-    path: 'home', component: HomeComponent
-  },
-  {
-    path: 'sobre', component: SobreComponent
-  },
-  {
-    path: 'contato', component: ContatoComponent
-  }
+  
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent }, 
+  { path: "cadastro", component: CadastroComponent },
+  { path: "login",    component: LoginComponent }
   
 ];
 
---
+++ PlanteUmaArvore-Front-Pi/src/app/model/Postagem.ts
@@ -0,0 +1,12 @@
+import { Tema } from './Tema';
+
+export class Postagem {
+    
+    public id: number;
+    public data: Date;
+    public privacidade: boolean;
+    public descricao: string;
+    public midia: Blob;
+    public tema: Tema;      //Chave estrangeira > Tb_Tema 
+    
+}
